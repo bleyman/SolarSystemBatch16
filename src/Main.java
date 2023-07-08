50fb6843135d81e505ebb673caacd42dc8e269c4
bleyman <130017588+bleyman@users.noreply.github.com>
2023-07-08 14:10:05 -0400

    Added sun but there is an error that still needs to be resolved

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         solarSystemBasic apples = new solarSystemBasic();
-        apples.Moon="2";
-        apples.Sun="1";
-        apples.Planets="2";
-        apples.Star="4";
+        apples.Moon = "2";
+        apples.Sun = "1";
+        apples.Planets = "2";
+        apples.Star = "4";
+
+        FeatureSun smoothie = new FeatureSun();
+        smoothie.name = "Banana";
+        smoothie.temp = 1564897 C;
+        smoothie.size = "456789km";
     }
 }
